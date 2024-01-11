df3e8b92a5b9d5fd058007999c43b8463067367d
crMinH <sungminhyoeng@naver.com>
2024-01-11 15:29:46 +0900

    테스트

--- src/main/java/com/example/board/controller/CommentController.java
+++ src/main/java/com/example/board/controller/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,11 +42,19 @@ public class CommentController {
 		return list;
 	}
 	
+	@PutMapping("/update")
+	public  Long updateComment(CommentReq prarams, Long c_sid) {
+		 commentService.updateComment(prarams);
+		 return commentService.deleteComment(c_sid);
+		
+	}
+	
 	// 댓글 삭제
 	@DeleteMapping("")
 	public String deleteComment(Long c_sid) {
 		commentService.deleteComment(c_sid);
 		return "삭제완료";
 	}
+	
 	
 }
